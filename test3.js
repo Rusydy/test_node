9f8dbb01be5d4e3623c85b1dcc6057b2d5a38aaa
Rusydy <rusydymuhiddin@gmail.com>
2022-05-20 07:42:51 +0700

    initial commit

--- test3.js
+++ test3.js
@@ -2,12 +2,6 @@ let currency_denominations = [
     100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100
 ]
 
-// get_denominations recieve two parameters:
-// 1. amount: the amount of money that you want to convert
-// 2. notes: how many notes you want to get
-// it will return an array of notes that sum up to equal or bigger than amount
-// e.g. get_denominations(23000, 4) will return [20000, 1000, 1000, 1000]
-
 function get_denominations(amount, notes) {
     let result = []
     let i = 0
